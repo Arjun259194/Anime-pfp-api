f862484dc425a58b6b6a1b623c3e03b97d669ced
arjun <arjun259194@gmail.com>
2022-04-19 20:56:29 +0530

    server is working, find a way to send img

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//initial commit
+const express = require('express') 
+const fs = require('fs')
+const data = require('./data.json')
+
+const port = 3000
+const app = express()
+
+function imgPicker(){
+  let RIndex = Math.ceil(Math.random()*20)
+  return data['anime-pfp'][RIndex]
+}
+
+app.get('/get',(req,res)=>{
+  res.type('text')
+  res.status(200).send(imgPicker())
+})
+
+app.listen(port,()=>{
+  console.log(`server running on http://127.0.0.1:${port}`);
+})
